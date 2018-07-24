6a5a2375aa9874c2af09b1232b30f1adc9ca6e14
Michal Drankowski <michaldrn@wp.pl>
2018-07-24 20:41:44 +0200

    showing brand name instead of category in home screen

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -21,60 +21,71 @@ export class ItemsComponent implements OnInit {
 
   constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute, public dialogRef: MatDialog) { }
 
-  ngOnInit() {
+    ngOnInit() {
 
-    this.products = [];
-    this.route.queryParams.subscribe(params => {
-        this.categoryName = params.category;
-        if(this.categoryName) {
-            this.url = "/api/items?category="+this.categoryName;
-        } else {
-            this.url = "/api/items";
-        }
-        var productsArr = [];
-        console.log("using url "+this.url);
-        this.http.get(this.url).subscribe(data => {
-            this.response = data;
-            this.response.products.forEach(product => {
-                product.fields.Price = product.fields.Price.toFixed(2);
-            });
-            this.response.products.forEach(product => {
-                if(typeof product.fields.Fotos[0] !== 'undefined') {
-                    productsArr.push(product);
+        this.products = [];
+        var makersUrl = "/api/makers";
+        var makersNameArr = {};
+        this.route.queryParams.subscribe(params => {
+            this.http.get(makersUrl).subscribe(data => {
+                this.response = data;
+                this.response.makers.forEach(maker => {
+                    makersNameArr[maker.id] = maker.fields.Name;
+                });
+                this.categoryName = params.category;
+                if (this.categoryName) {
+                    this.url = "/api/items?category=" + this.categoryName;
+                } else {
+                    this.url = "/api/items";
                 }
+                var productsArr = [];
+                this.http.get(this.url).subscribe(data => {
+                    this.response = data;
+                    this.response.products.forEach(product => {
+                        product.fields.brandName = makersNameArr[product.fields.Maker];
+                    });
+                    this.response.products.forEach(product => {
+                        product.fields.Price = product.fields.Price.toFixed(2);
+                    });
+                    this.response.products.forEach(product => {
+                        if (typeof product.fields.Fotos[0] !== 'undefined') {
+                            productsArr.push(product);
+                        }
+                    });
+                    this.products = productsArr;
+                }, err => {
+                    if (err.status === 401) {
+                        //this.router.navigate(['login']);
+                    }
+                });
             });
-         this.products = productsArr;
-       
-      //console.log(this.products);
-  }, err => {
-      if(err.status === 401) {
-          //this.router.navigate(['login']);
-      }
-  });
-});
-}
+        });
+
+    }
 
 
-addToCart(idProduct) {
-    this.http.get('/api/add-item-to-cart/'+idProduct).subscribe(data => {
-        this.response = data;
-        console.log(this.response);
-        this.dialogRef.open(AddedToCartDialogComponent, {
-            height: '146px',
-            width: '400px',
-          });
-    });
-}
+    addToCart(idProduct) {
+        this.http.get('/api/add-item-to-cart/' + idProduct).subscribe(data => {
+            this.response = data;
+            console.log(this.response);
+            this.dialogRef.open(AddedToCartDialogComponent, {
+                height: '146px',
+                width: '400px',
+            });
+        });
+    }
 
 
-addToWishlist(idProduct) {
-    let httpOptions = {
-        headers: new HttpHeaders({ 'Authorization': localStorage.getItem('jwtToken') })
-      };
-    this.http.get('/api/add-item-to-wishlist/'+idProduct, httpOptions).subscribe(data => {
-        this.response = data;
-        console.log(this.response);
-    });
-}
+    addToWishlist(idProduct) {
+        let httpOptions = {
+            headers: new HttpHeaders({
+                'Authorization': localStorage.getItem('jwtToken')
+            })
+        };
+        this.http.get('/api/add-item-to-wishlist/' + idProduct, httpOptions).subscribe(data => {
+            this.response = data;
+            console.log(this.response);
+        });
+    }
 
 }
